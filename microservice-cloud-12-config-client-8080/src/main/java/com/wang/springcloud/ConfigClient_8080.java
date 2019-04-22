8406c6b8b259b2cd18bfa55fb0a24da8bf203828
wangtong <wangtong@mail.39.net>
2019-04-22 15:17:43 +0800

    SpringConfig：客户端服务（成功读取配置中心配置的git上的文件中的属性）

---
+++ microservice-cloud-12-config-client-8080/src/main/java/com/wang/springcloud/ConfigClient_8080.java
@@ -0,0 +1,16 @@
+package com.wang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by wangtong on 2019/4/22.
+ */
+@SpringBootApplication
+public class ConfigClient_8080 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient_8080.class, args);
+    }
+
+}
