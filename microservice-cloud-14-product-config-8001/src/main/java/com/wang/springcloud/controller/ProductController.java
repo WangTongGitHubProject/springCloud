b6c50eed7bf3d2e85c17a229e462978317d17051
wangtong <wangtong@mail.39.net>
2019-04-23 16:32:10 +0800

    Spring Cloud Bus 实战： 1：自定义类中读取配置（@RefreshScope） 2：刷新Druid数据源配置（数据源特例）

--- microservice-cloud-14-product-config-8001/src/main/java/com/wang/springcloud/controller/ProductController.java
+++ microservice-cloud-14-product-config-8001/src/main/java/com/wang/springcloud/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.wang.springcloud.controller;
 import com.wang.springcloud.entities.Product;
 import com.wang.springcloud.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,11 +12,20 @@ import java.util.List;
 /**
  * Created by wangtong on 2019/4/10.
  */
+@RefreshScope //刷新配置
 @RestController
 public class ProductController {
 
     @Autowired
     private ProductService productService;
+
+    @Value("${emp.name}")
+    private String name;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return name;
+    }
 
     @RequestMapping(value = "/product/add", method = RequestMethod.POST)
     public boolean add(@RequestBody Product product) {
